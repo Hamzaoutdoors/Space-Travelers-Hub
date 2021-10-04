b5d4e9800f4031fcda7ab382532c8200637a7b74
Hamza <hamzaoutdoors@gmail.com>
2021-10-05 00:33:24 +0100

    Remove padding for navbar

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import './App.css';
 const App = () => (
   <Router>
     <Header />
-    <main>
+    <main bg="light">
       <Switch>
         <Route path="/" exact>
           <Rockets />
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import logo from '../media/planet.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => (
-  <Navbar bg="light" expand="lg">
+  <Navbar bg="light" expand="lg" className="py-0">
     <Container className="nav-wrapper">
       <Navbar.Brand href="#home" className="App-header d-flex align-items-center">
         <img src={logo} className="App-logo" alt="logo" />
