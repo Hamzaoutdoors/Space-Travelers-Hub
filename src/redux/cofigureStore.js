a24650fb806070b04d6746d6a795494e84672f73
Hamza <hamzaoutdoors@gmail.com>
2021-10-04 18:50:40 +0100

    Add redux following 'feature folder'

---
+++ src/redux/cofigureStore.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rocketsReducer from './rockets/rockets';
+
+const reducer = combineReducers({
+  rocketsReducer,
+  // additional reducers could be added here
+});
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(logger, thunk)),
+);
+
+export default store;
