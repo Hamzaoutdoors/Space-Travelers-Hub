9e6cf37f61d3d4bfe1a7718271304ccc83f76649
Lucas Ferrari Soto <lcsfs11@gmail.com>
2021-10-05 15:56:39 -0300

    Add missions redux duck

---
+++ src/redux/missions/missions.js
@@ -0,0 +1,32 @@
+import fetchMissions from '../../utils/missions-api';
+
+const FETCH_MISSIONS = 'spaceX/missions/FETCH_MISSIONS';
+const ADD_MISSIONS = 'spaceX/missions/ADD_MISSIONS';
+const API_SUCCESS = 'spaceX/missions/API_SUCCESS';
+const API_FAILURE = 'spaceX/missions/API_FAILURE';
+
+const initialMissions = [];
+
+export const fetchMissionsAction = () => (dispatch) => {
+  dispatch({ type: FETCH_MISSIONS });
+  return fetchMissions().then(
+    (missions) => {
+      dispatch({ type: API_SUCCESS });
+      dispatch({ type: ADD_MISSIONS, missions })
+    },
+    (error) => {
+      dispatch({ type: API_FAILURE, error });
+    }
+  );
+}
+
+const missionsReducer = (state = initialMissions, action) => {
+  switch (action.type) {
+  case ADD_MISSIONS:
+    return [...state, ...action.missions];
+  default:
+    return state;
+  }
+};
+
+export default missionsReducer;
--- src/utils/missions-api.js
+++ src/utils/missions-api.js
@@ -7,12 +7,15 @@ const config = {
 
 const fetchMissions = async () => {
   const response = await axios(config);
+  // Non camelCase are needed here since they're used by the missions API.
+  /* eslint-disable camelcase */
   return response.data.map(({ mission_id, mission_name, description }) => ({
     id: mission_id,
     name: mission_name,
     description,
     status: 'Not a member',
   }));
+  /* eslint-enable camelcase */
 };
 
 export default fetchMissions;
