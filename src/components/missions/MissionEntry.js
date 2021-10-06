b0ed7fdcf09b9b94e5e248facc8c504bca169678
Lucas Ferrari Soto <lcsfs11@gmail.com>
2021-10-06 15:02:43 -0300

    Refactor StatusBadge to be a proper component and use conditional rendering

--- src/components/missions/MissionEntry.js
+++ src/components/missions/MissionEntry.js
@@ -2,10 +2,17 @@ import PropTypes from 'prop-types';
 import { Badge } from 'react-bootstrap';
 import JoinButton from './JoinButton';
 
-const statusBadge = (status) => {
-  const bg = status === 'Not a member' ? 'secondary' : 'primary';
-  const classes = status === 'Not a member' ? 'mx-2 text-uppercase' : 'mx-2';
-  return <Badge className={classes} bg={bg}>{status}</Badge>;
+const StatusBadge = ({ status }) => (
+  <Badge
+    className={`mx-2 ${status === 'Not a member' && 'text-uppercase'}`}
+    bg={status === 'Not a member' ? 'secondary' : 'primary'}
+  >
+    {status}
+  </Badge>
+);
+
+StatusBadge.propTypes = {
+  status: PropTypes.oneOf(['Not a member', 'Active member']).isRequired,
 };
 
 const MissionEntry = ({
@@ -14,7 +21,7 @@ const MissionEntry = ({
   <tr key={id}>
     <td>{name}</td>
     <td>{description}</td>
-    <td className="align-middle">{statusBadge(status)}</td>
+    <td className="align-middle"><StatusBadge status={status} /></td>
     <td className="align-middle"><JoinButton status={status} id={id} /></td>
   </tr>
 );
