e1eb712e552c7d70ea10e9f0ed964d088ca65ce3
Hamza <hamzaoutdoors@gmail.com>
2021-10-06 18:48:25 +0100

    Add functionality to switch badge rocket

--- src/components/rockets/RocketsList.js
+++ src/components/rockets/RocketsList.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import { motion } from 'framer-motion';
 import Rocket from './Rocket';
+
+const container = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+      delayChildren: 0.3,
+    },
+  },
+};
 
 const Rockets = () => {
   const rockets = useSelector((state) => state.rockets);
@@ -26,7 +40,13 @@ const Rockets = () => {
   return (
     <>
       <Container>
-        {rocketsList}
+        <motion.div
+          variants={container}
+          initial="initial"
+          animate="animate"
+        >
+          {rocketsList}
+        </motion.div>
       </Container>
     </>
   );
