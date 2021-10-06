40ce2bb5f59743abc53d799562633a5364b4e3ad
Hamza <hamzaoutdoors@gmail.com>
2021-10-06 01:29:25 +0100

    Write actions and reducers for booking rockets

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -4,6 +4,7 @@ const FETCH_ROCKETS = 'spaceX/rockets/FETCH_ROCKETS';
 const ADD_ROCKETS = 'spaceX/rockets/ADD_ROCKETS';
 const API_SUCCESS = 'spaceX/rockets/API_SUCCESS';
 const API_FAILURE = 'spaceX/rockets/API_FAILURE';
+const RESERVE_ROCKET = 'spaceX/rockets/RESERVE_ROCKET';
 
 const initialRockets = [];
 
@@ -20,10 +21,23 @@ export const fetchRocketsAction = () => (dispatch) => {
   );
 };
 
+export const bookRocket = (currentState, id) => (dispatch) => {
+  const newState = currentState.map((rocket) => {
+    if (rocket.id !== id) return rocket;
+    return { ...rocket, reserved: true };
+  });
+  dispatch({
+    type: RESERVE_ROCKET,
+    newState,
+  });
+};
+
 const rocketsReducer = (state = initialRockets, action) => {
   switch (action.type) {
     case ADD_ROCKETS:
       return [...state, ...action.rockets];
+    case RESERVE_ROCKET:
+      return action.newState;
     default:
       return state;
   }
