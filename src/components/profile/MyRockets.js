e72fc965484c5859ab1f77bb245ca3637a8722f9
Hamza <hamzaoutdoors@gmail.com>
2021-10-06 21:39:24 +0100

    Add MyRockets components

--- src/components/profile/MyRockets.js
+++ src/components/profile/MyRockets.js
@@ -1,5 +1,27 @@
-const MyRockets = () => (
-  <h2>My Rockets</h2>
-);
+import { Table } from 'react-bootstrap';
+import { useSelector, shallowEqual } from 'react-redux';
+
+const MyRockets = () => {
+  const rockets = useSelector((state) => state.rockets, shallowEqual);
+
+  return (
+    <div className="my-missions">
+      <h2 className="mb-4">My Rockets</h2>
+      <Table bordered hover size="sm" responsive>
+        <tbody>
+          {
+            rockets.filter(({ reserved }) => reserved).map(({ name, id }) => (
+              <>
+                <tr key={id}>
+                  <td><p className="p-3">{name}</p></td>
+                </tr>
+              </>
+            ))
+          }
+        </tbody>
+      </Table>
+    </div>
+  );
+};
 
 export default MyRockets;
